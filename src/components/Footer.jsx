898b56edc69a80803f18b257754c04f17d9d1ce7
Neha Sellappan Devadas <n.devadas@iitg.ac.in>
2025-06-11 00:28:33 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='flex justify-center items-center h-[4.5rem] bg-black text-white bottom-0 w-full'>
-      <div>Copyright &copy; 2024 Made by Kushanava Ghosh. All rights reserved</div>
+      <div></div>
     </footer>
   )
 }
